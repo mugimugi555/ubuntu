60596fd31673312a83c713dbfcfe3ff85140e695
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-14 15:12:54 +0900

    Update install_laravel.sh

--- install_laravel.sh
+++ install_laravel.sh
@@ -4,14 +4,12 @@
 
 cd;
 sudo echo;
-sudo apt install -y apache2 php php-xml mariadb-server composer ;
+sudo apt install -y apache2 php php-xml mariadb-server ;
 
-# or install compooser latest ver2.x manuary
-# php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-# php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-# php composer-setup.php
-# php -r "unlink('composer-setup.php');"
-# sudo mv composer.phar /usr/local/bin/composer
+#
+php -r "copy ( 'https://getcomposer.org/installer', 'composer-setup.php' ) ;" ;
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer ;
+composer -v ;
 
 #
 sudo mv /var/www/html/ /var/www/html_back ;

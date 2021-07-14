9cf7fe2016f9b77c997f117c0c6e57d848ba1d6d
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-14 15:18:37 +0900

    Update install_yarn.sh

--- install_yarn.sh
+++ install_yarn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#
+# wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_yarn.sh && bash install_yarn.sh ;
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - ;
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list ;

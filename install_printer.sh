9f92242a246690f856ec159052da530a40c1482f
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-17 21:24:56 +0900

    Update install_printer.sh

--- install_printer.sh
+++ install_printer.sh
@@ -5,29 +5,43 @@
 cd ;
 sudo echo ;
 
-echo "===================";
+echo "=================================";
+echo "";
 echo "install libraries for compile";
+echo "";
+echo "=================================";
 sudo apt install gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups ;
 sudo apt-get install libcups2-dev ;
 echo "";
 
-echo "===================";
+echo "=================================";
+echo "";
 echo "download file and extract";
+echo "";
+echo "=================================";
 wget https://jaist.dl.sourceforge.net/project/gimp-print/gutenprint-5.3/5.3.4/gutenprint-5.3.4.tar.bz2 ;
 tar xvf gutenprint-5.3.4.tar.bz2 ;
 cd gutenprint-5.3.4/ ;
 echo "";
 
-echo "===================";
+echo "=================================";
+echo "";
 echo "configure and make and make install";
+echo "";
+echo "=================================";
 ./configure ;
 make clean ;
 make ;
 sudo make install ;
 echo "";
 
-echo "===================";
+echo "=================================";
+echo "";
 echo "cups install done";
 echo "";
+echo "=================================";
+echo "";
 echo "please install downlad manuary";
+echo "";
+echo "=================================";
 gnome-control-center printers &

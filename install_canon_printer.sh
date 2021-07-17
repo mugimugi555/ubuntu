fa98c8798c13a404cbda65237021ac609ccc52ed
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-17 21:13:01 +0900

    Create install_canon_printer.sh

---
+++ install_canon_printer.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+#
+
+cd ;
+sudo echo ;
+
+echo "===================";
+echo "install libraries for compile";
+sudo apt install gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups ;
+sudo apt-get install libcups2-dev ;
+echo "";
+
+echo "===================";
+echo "download file and extract";
+wget https://jaist.dl.sourceforge.net/project/gimp-print/gutenprint-5.3/5.3.4/gutenprint-5.3.4.tar.bz2 ;
+tar xvf  gutenprint* ;
+cd gutenprint* ;
+echo "";
+
+echo "===================";
+echo "configure and make and make install";
+./configure ;
+make clean ;
+make ;
+sudo make install ;
+echo "";
+
+echo "===================";
+echo "cups install done";

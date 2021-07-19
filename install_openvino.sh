5ad8b1492e8b9bb97e1d8fbcb4fa4cf538e846c5
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2021-07-19 21:31:03 +0900

    Create install_openvino.sh

---
+++ install_openvino.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+#
+
+#
+echo "deb https://apt.repos.intel.com/openvino/2021 all main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2021.list ;
+wget https://apt.repos.intel.com/openvino/2021/GPG-PUB-KEY-INTEL-OPENVINO-2021 ;
+sudo apt-key add GPG-PUB-KEY-INTEL-OPENVINO-2021  ;
+sudo apt update ;
+apt-cache search openvino ;
+sudo apt-cache search intel-openvino-runtime-ubuntu20 ;
+
+#
+sudo apt install -y intel-openvino-dev-ubuntu20-2021.4.582 ;

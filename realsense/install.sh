0a2fcbe49b400d630e5de58c510040972e28f7a5
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-01 09:53:38 +0900

    Update install.sh

--- realsense/install.sh
+++ realsense/install.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# wget 
+
 sudo echo ;
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE ;

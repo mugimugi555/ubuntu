c349c26d71734e17de09b75a6ed381052631b759
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-10 19:43:04 +0900

    Update install.sh

--- onkyo/install.sh
+++ onkyo/install.sh
@@ -6,5 +6,5 @@ sudo apt install -y grub2-common grub-efi-ia32 ;
 sudo grub-install --efi-directory=/boot/efi ;
 sudo update-grub ;
 
-sudo sed -ei "s/quiet splash\”/quiet splash nomodeset\”/" /etc/default/grub ;
+sudo sed -ei 's/quiet splash”/quiet splash nomodeset”/' /etc/default/grub ;
 sudo update-grub2 ;

54bc5468d979d40fba04efd813ae0cea17b1d9eb
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-21 17:20:18 +0900

    Update install_chromedriver_service.sh

--- install_chromedriver_service.sh
+++ install_chromedriver_service.sh
@@ -5,22 +5,21 @@
 cd ;
 
 # install nodejs npm
-wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_nodejs.sh && bash install_nodejs.sh
+#wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_nodejs.sh && bash install_nodejs.sh
 
 # install chromedriver
-wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_chromedriver.php && php install_chromedriver.php ;
-
-echo "";
-echo "========== install chrome driver service to pm2 ======";
-echo "";
+#wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_chromedriver.php && php install_chromedriver.php ;
 
 # install pm2 service
 sudo npm install pm2 -g ;
 
 # add services
-pm2 start "/home/$USER/chromedrivers/chromedriver_89 --port=5089" --name=chromedriver_89_5089 ;
-pm2 start "/home/$USER/chromedrivers/chromedriver_90 --port=5090" --name=chromedriver_90_5090 ;
-pm2 start "/home/$USER/chromedrivers/chromedriver_91 --port=5091" --name=chromedriver_91_5091 ;
+pm2 start "/home/$USER/chromedrivers/chromedriver     --port=5001" --name=chromedriver_latest_5001 ;
+pm2 start "/home/$USER/chromedrivers/chromedriver_97  --port=5097" --name=chromedriver_97_5097     ;
+pm2 start "/home/$USER/chromedrivers/chromedriver_98  --port=5098" --name=chromedriver_98_5098     ;
+pm2 start "/home/$USER/chromedrivers/chromedriver_99  --port=5099" --name=chromedriver_99_5099     ;
+pm2 start "/home/$USER/chromedrivers/chromedriver_109 --port=5100" --name=chromedriver_200_5100    ;
+
 pm2 save ;
 
 # auto start at logon

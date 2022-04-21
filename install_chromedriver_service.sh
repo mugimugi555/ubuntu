83269e84a08e81f58b51267391cefb38252673fc
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-21 17:23:05 +0900

    Update install_chromedriver_service.sh

--- install_chromedriver_service.sh
+++ install_chromedriver_service.sh
@@ -2,8 +2,6 @@
 
 # wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_chromedriver_service.sh && bash install_chromedriver_service.sh ;
 
-cd ;
-
 # install nodejs npm
 #wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_nodejs.sh && bash install_nodejs.sh
 
@@ -15,30 +13,23 @@ sudo npm install pm2 -g ;
 
 # add services
 pm2 start "/home/$USER/chromedrivers/chromedriver     --port=5001" --name=chromedriver_latest_5001 ;
-pm2 start "/home/$USER/chromedrivers/chromedriver_97  --port=5097" --name=chromedriver_97_5097     ;
-pm2 start "/home/$USER/chromedrivers/chromedriver_98  --port=5098" --name=chromedriver_98_5098     ;
-pm2 start "/home/$USER/chromedrivers/chromedriver_99  --port=5099" --name=chromedriver_99_5099     ;
-pm2 start "/home/$USER/chromedrivers/chromedriver_109 --port=5100" --name=chromedriver_200_5100    ;
+#pm2 start "/home/$USER/chromedrivers/chromedriver_97  --port=5097" --name=chromedriver_97_5097     ;
+#pm2 start "/home/$USER/chromedrivers/chromedriver_98  --port=5098" --name=chromedriver_98_5098     ;
+#pm2 start "/home/$USER/chromedrivers/chromedriver_99  --port=5099" --name=chromedriver_99_5099     ;
+#pm2 start "/home/$USER/chromedrivers/chromedriver_109 --port=5100" --name=chromedriver_200_5100    ;
 
 pm2 save ;
 
 # auto start at logon
-echo "";
-echo "========== please hit the next command ======";
-echo "";
 pm2 startup ;
-echo "";
 sudo env PATH=$PATH:/usr/local/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
 
 pm2 status ;
 
-xdg-open http://localhost:5089 &
-xdg-open http://localhost:5090 &
-xdg-open http://localhost:5091 &
+#xdg-open http://localhost:5089 &
+#xdg-open http://localhost:5090 &
+#xdg-open http://localhost:5091 &
 
-echo "";
-echo "here is a chrome drivers address";
-echo "";
-echo "http://localhost:5089";
-echo "http://localhost:5090";
-echo "http://localhost:5091";
+#echo "http://localhost:5089";
+#echo "http://localhost:5090";
+#echo "http://localhost:5091";

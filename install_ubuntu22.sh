f554ec78e1211a96402580d1677a68a1cee20e86
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-23 03:35:27 +0900

    Update install_ubuntu22.sh

--- install_ubuntu22.sh
+++ install_ubuntu22.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
+# wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_ubuntu22.sh && bash install_ubuntu22.sh ;
+
 sudo apt update ;
 sudo apt upgrade -y ;
 sudo apt install update-manager ;
 sudo apt dist-upgrade -y ;
 sudo do-release-upgrade -c -d -y ;
+sudo reboot now ;

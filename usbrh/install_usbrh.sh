127ea899a67b3b89b9a1ecceb6438ecd29aa124d
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-14 14:48:59 +0900

    Update install_usbrh.sh

--- usbrh/install_usbrh.sh
+++ usbrh/install_usbrh.sh
@@ -1,10 +1,11 @@
 !#/usr/bin/bash
 
-# wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_usbrh.sh && bash install_usbrh.sh ;
+# wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/usbrh/install_usbrh.sh && bash install_usbrh.sh ;
 
 sudo echo ;
 sudo apt install -y build-essential ;
 sudo apt install -y libusb-dev ;
+
 cd ;
 git clone https://github.com/osapon/usbrh-linux.git ;
 cd usbrh-linux/ ;

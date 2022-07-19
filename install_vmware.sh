054d3e78d41bb866229d3eec0a66398033fe653f
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-19 14:15:14 +0900

    Update install_vmware.sh

--- install_vmware.sh
+++ install_vmware.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_vmware.sh && bash install_vmware.sh ;
+
 # Install VMware Workstation Player on Ubuntu 22.04 LTS - Linux Shout 
 # https://www.how2shout.com/linux/install-vmware-workstation-player-on-ubuntu-22-04-lts/
 

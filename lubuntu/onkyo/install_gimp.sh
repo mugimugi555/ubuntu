bb9c91fe28aa76069c31cc7c23b09ed3e6361bb0
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-21 13:17:30 +0900

    Update install_gimp.sh

--- lubuntu/onkyo/install_gimp.sh
+++ lubuntu/onkyo/install_gimp.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+# wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/lubuntu/onkyo/install_gimp.sh && bash install_gimp.sh ;
+
 sudo echo ;
 
 sudo apt install -y gimp ;

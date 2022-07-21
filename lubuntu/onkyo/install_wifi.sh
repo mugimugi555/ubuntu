f2d5c4c2a58d4e591bfb0b1a02ce3b74acc7ba2c
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-21 16:50:42 +0900

    Update install_wifi.sh

--- lubuntu/onkyo/install_wifi.sh
+++ lubuntu/onkyo/install_wifi.sh
@@ -8,5 +8,9 @@ sudo echo ;
 # wifi driver
 #-----------------------------------------------------------------------------------------------------------------------
 # https://packages.debian.org/ja/sid/all/firmware-brcm80211/download
-wget http://ftp.jp.debian.org/debian/pool/non-free/f/firmware-nonfree/firmware-brcm80211_20210818-1_all.deb ;
-sudo dpkg -i firmware-brcm80211_20210818-1_all.deb ;
+#wget http://ftp.jp.debian.org/debian/pool/non-free/f/firmware-nonfree/firmware-brcm80211_20210818-1_all.deb ;
+#sudo dpkg -i firmware-brcm80211_20210818-1_all.deb ;
+
+sudo apt-add-repository non-free ;
+sudo apt update ;
+sudo apt upgrade -y ;

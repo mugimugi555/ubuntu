6418ed6783b5503ddfb2aabc356d2a5b58f94ade
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-22 10:26:19 +0900

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -4,9 +4,20 @@
 
 sudo echo;
 
+#
 sudo apt install -y nodejs npm ;
+node -v ;
+npm  -v ;
+
+#
 sudo npm install n -g ;
 sudo n stable ;
-sudo apt purge -y nodejs npm ;
-#exec $SHELL -l ;
+sudo npm install -g yarn ;
+sudo apt purge -y nodejs ;
+sudo apt autoremove -y ;
+exec $SHELL -l ;
+
+#
 node -v ;
+npm  -v ;
+yarn -v ;

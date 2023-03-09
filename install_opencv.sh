196029c7e8a4206eb3a4a63a8ae7b99a14a0659a
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2023-03-09 17:02:44 +0900

    Update install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/bash
+
+# install cmd
+# wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_opencv.sh && bash install_opencv.sh ;
+
+# sudo
+sudo echo ;
+
 sudo apt install -y python3-pip ;
 pip3 install opencv-python ;
 pip3 install opencv-contrib-python ;

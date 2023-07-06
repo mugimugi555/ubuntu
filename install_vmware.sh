207b0d708e2561156b4dc603906318422dc16769
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2023-07-06 19:29:14 +0900

    Update install_vmware.sh

--- install_vmware.sh
+++ install_vmware.sh
@@ -2,21 +2,19 @@
 
 wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_vmware.sh && bash install_vmware.sh ;
 
-# Install VMware Workstation Player on Ubuntu 22.04 LTS - Linux Shout 
-# https://www.how2shout.com/linux/install-vmware-workstation-player-on-ubuntu-22-04-lts/
-
-xdg-open https://www.vmware.com/in/products/workstation-player.html ;
-
 sudo apt install -y gcc ;
 sudo apt install -y build-essential ;
 sudo apt install -y unzip ;
 
-cd ;
-wget https://codeload.github.com/mkubecek/vmware-host-modules/zip/refs/tags/w16.2.3-k5.18 ;
-unzip w16.2.3-k5.18 ;
-cd vmware-host-modules-w16.2.3-k5.18 ;
-tar -cf vmmon.tar vmmon-only ;
-tar -cf vmnet.tar vmnet-only ;
-sudo cp -v vmmon.tar vmnet.tar /usr/lib/vmware/modules/source/ ;
+xdg-open "https://customerconnect.vmware.com/en/downloads/details?downloadGroup=WKST-PLAYER-1702&productId=1377&rPId=104734" ;
+sudo sh VMware-Player-Full-17.0.2-21581411.x86_64.bundle ;
 
-sudo vmware-modconfig --console --install-all ;
+# https://github.com/mkubecek/vmware-host-modules/blob/master/INSTALL
+cd ;
+wget https://github.com/mkubecek/vmware-host-modules/archive/workstation-17.0.2.tar.gz ;
+tar -xzf workstation-17.0.2.tar.gz ;
+cd vmware-host-modules-workstation-17.0.2 ;
+make ;
+sudo make install ;
+
+vmplayer & ;

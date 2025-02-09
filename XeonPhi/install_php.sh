336fc061d69e417aaade3e670bdfbb30b1b76150
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2025-02-09 18:21:53 +0900

    Create install_php.sh

---
+++ XeonPhi/install_php.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "=== PHP をビルドして Xeon Phi に転送 ==="
+
+# 必要なパッケージをホストPCにインストール
+sudo apt update
+sudo apt install -y build-essential autoconf bison re2c \
+                    libxml2-dev libsqlite3-dev libcurl4-openssl-dev \
+                    libjpeg-dev libpng-dev libxpm-dev libfreetype6-dev libzip-dev
+
+# PHP のソースコードを取得
+cd /tmp
+wget https://www.php.net/distributions/php-8.2.6.tar.gz
+tar xvf php-8.2.6.tar.gz
+cd php-8.2.6
+
+# PHP をビルド
+./configure --prefix=/home/mic/php --disable-all --enable-cli
+make -j$(nproc)
+make install
+
+# Xeon Phi に転送
+scp -r /home/mic/php mic0:/home/mic/
+
+# 動作確認
+ssh mic0 "/home/mic/php/bin/php -v"
+
+echo "=== PHP のインストール完了 ==="

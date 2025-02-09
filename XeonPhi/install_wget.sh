e570bbdfe294d49cb183bf7f0d0ecb2f08053f79
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2025-02-09 18:24:24 +0900

    Update install_wget.sh

--- XeonPhi/install_wget.sh
+++ XeonPhi/install_wget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "=== wget をビルドして Xeon Phi に転送 ==="
+echo "=== wget を静的リンクでビルド & Xeon Phi に転送 ==="
 
 # 必要なパッケージをホストPCにインストール
 sudo apt update
@@ -12,8 +12,8 @@ wget https://ftp.gnu.org/gnu/wget/wget-1.21.3.tar.gz
 tar xvf wget-1.21.3.tar.gz
 cd wget-1.21.3
 
-# wget をビルド
-./configure --prefix=/home/mic/wget --with-ssl=openssl
+# wget を静的リンクでビルド
+./configure --prefix=/home/mic/wget --with-ssl=openssl LDFLAGS="-static"
 make -j$(nproc)
 make install
 

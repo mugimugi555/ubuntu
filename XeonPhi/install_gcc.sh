b0db6c0d8a560dc45b38547161fe1c555887e35a
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2025-02-09 22:09:09 +0900

    Update install_gcc.sh

--- XeonPhi/install_gcc.sh
+++ XeonPhi/install_gcc.sh
@@ -5,12 +5,19 @@ echo "=== Xeon Phi に GCC & CMake (静的リンク版) をインストール ==
 # バージョン指定
 GCC_VERSION="13.1.0"
 CMAKE_VERSION="3.27.0"
-GCC_INSTALL_DIR="/home/mic/gcc-${GCC_VERSION}"
-CMAKE_INSTALL_DIR="/home/mic/cmake-${CMAKE_VERSION}"
+
+# 必要なパッケージをホストPCにインストール
+sudo apt update
+sudo apt install -y build-essential wget
 
 ### 1️⃣ ホスト側で GCC のビルド & 転送 ###
 echo "=== ホスト側で GCC ${GCC_VERSION} のビルド開始 ==="
-cd /tmp
+
+# 作業ディレクトリの作成（ビルドは /tmp で実施）
+mkdir -p /tmp/gcc_build
+cd /tmp/gcc_build
+
+# GCC のソースコードを取得
 wget https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.gz
 tar xzf gcc-${GCC_VERSION}.tar.gz
 cd gcc-${GCC_VERSION}
@@ -23,7 +30,7 @@ mkdir build
 cd build
 
 # コンパイル設定（静的リンク有効化）
-../configure --prefix=${GCC_INSTALL_DIR} \
+../configure --prefix=$HOME/mic/bin/gcc-${GCC_VERSION} \
              --enable-languages=c,c++ \
              --disable-multilib \
              --enable-static \
@@ -33,37 +40,54 @@ cd build
 make -j$(nproc)
 make install
 
-# Xeon Phi に転送
+# GCC を Xeon Phi に転送
 echo "=== GCC を Xeon Phi に転送 ==="
-scp -r ${GCC_INSTALL_DIR} mic0:/home/mic/
+scp -r $HOME/mic/bin mic0:/home/mic/bin/
+
+# 作業ディレクトリの削除（オプション）
+rm -rf /tmp/gcc_build
+
+echo "=== GCC のビルド & クリーンアップ完了 ==="
+
+---
 
 ### 2️⃣ ホスト側で CMake のビルド & 転送 ###
 echo "=== ホスト側で CMake ${CMAKE_VERSION} のビルド開始 ==="
-cd /tmp
+
+# 作業ディレクトリの作成（ビルドは /tmp で実施）
+mkdir -p /tmp/cmake_build
+cd /tmp/cmake_build
+
+# CMake のソースコードを取得
 wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz
 tar xzf cmake-${CMAKE_VERSION}.tar.gz
 cd cmake-${CMAKE_VERSION}
 
 # ビルド & インストール
-./bootstrap --prefix=${CMAKE_INSTALL_DIR} -- -DCMAKE_BUILD_TYPE=Release
+./bootstrap --prefix=$HOME/mic/bin/cmake-${CMAKE_VERSION} -- -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 make install
 
-# Xeon Phi に転送
+# CMake を Xeon Phi に転送
 echo "=== CMake を Xeon Phi に転送 ==="
-scp -r ${CMAKE_INSTALL_DIR} mic0:/home/mic/
+scp -r $HOME/mic/bin mic0:/home/mic/bin/
+
+# 作業ディレクトリの削除（オプション）
+rm -rf /tmp/cmake_build
+
+echo "=== CMake のビルド & クリーンアップ完了 ==="
+
+---
 
 ### 3️⃣ Xeon Phi 側で環境変数の設定 & 確認 ###
 echo "=== Xeon Phi 側で環境変数を設定 & インストール確認 ==="
 ssh mic0 << 'EOF'
     GCC_VERSION="13.1.0"
     CMAKE_VERSION="3.27.0"
-    GCC_INSTALL_DIR="/home/mic/gcc-${GCC_VERSION}"
-    CMAKE_INSTALL_DIR="/home/mic/cmake-${CMAKE_VERSION}"
 
     # 環境変数を設定
-    echo "export PATH=${GCC_INSTALL_DIR}/bin:${CMAKE_INSTALL_DIR}/bin:\$PATH" >> ~/.bashrc
-    echo "export LD_LIBRARY_PATH=${GCC_INSTALL_DIR}/lib64:\$LD_LIBRARY_PATH" >> ~/.bashrc
+    echo "export PATH=/home/mic/bin/gcc-${GCC_VERSION}/bin:/home/mic/bin/cmake-${CMAKE_VERSION}/bin:\$PATH" >> ~/.bashrc
+    echo "export LD_LIBRARY_PATH=/home/mic/bin/gcc-${GCC_VERSION}/lib64:\$LD_LIBRARY_PATH" >> ~/.bashrc
     source ~/.bashrc
 
     # インストール確認
